818d4525f09bd2891e4a4faf83ecd3679616abdc
sschumm <sven.schumm7@gmail.com>
2022-07-12 10:57:25 +0200

    Optimized build() in model.py
    The size of the Matrix M is now computed of the boundary conditions.
    
    The increment to apply the boundaries is depending on the no of boundaries per layer

--- Moduls_Tests/model.py
+++ Moduls_Tests/model.py
@@ -51,7 +51,8 @@ class Model():
         for i in range(self.n_layers-1):
             self.layers[i].sync_layer_info(self.layers[i+1].mu_inv)
         
-        n_vars = self.n_layers * 2 + 2
+        n_bounds = sum([len(vals.bounds) for vals in self.layers.values()])
+        n_vars = n_bounds + 2
         
         self.M = np.zeros((n_vars, n_vars))
         self.y = np.zeros(n_vars)
@@ -60,32 +61,13 @@ class Model():
         self.M[-1, -2] = 1
         
         
-        for i in range(self.n_layers):
-            
-            # j = i+1
-            # r = self.layers[i].r
-            # mu_inv1 = self.layers[i].mu_inv
-            
-# =============================================================================
-#             if j == self.n_layers:
-#                 mu_inv2 = 1 / mu_0
-#             else:
-#                 mu_inv2 = self.layers[j].mu_inv
-# =============================================================================
+        i = 0
+        for layer in self.layers.values():
+                      
+            self.M = layer.apply_boundaries(self.M, i)
+            i += len(layer.bounds)
         
-            # B_r = const.
-            self.M = self.layers[i].apply_boundaries(self.M, i)
-# =============================================================================
-#             self.M[i+1, i:i+4] = np.array([-r**self.p, -r**-self.p, 
-#                                             r**self.p,  r**-self.p])
-# =============================================================================
-            # H_0 = const.
-            
-# =============================================================================
-#             self.M[i+2, i:i+4] = np.array([-mu_inv1 * r**(self.p-1),  mu_inv1 * r**-(self.p+1),
-#                                             mu_inv2 * r**(self.p-1), -mu_inv2 * r**-(self.p+1)]) * self.p
-# =============================================================================
-            
+
         
             
         
