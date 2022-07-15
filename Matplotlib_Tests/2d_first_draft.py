65be33ff3e9545f80a3b176b650a1047bab0c8f4
sschumm <sven.schumm7@gmail.com>
2022-07-15 23:36:21 +0200

    Put streamplot and quiver into the figure of circles

--- Matplotlib_Tests/2d_first_draft.py
+++ Matplotlib_Tests/2d_first_draft.py
@@ -12,10 +12,11 @@ r = np.array([1, 2, 3, 4])
 c = ["black", "blue", "red", "green"]
 lim = np.max(r) * 1.2
 
-fig, ax = plt.subplots(1)
+plt.figure(figsize=(10, 10))
+ax = plt.subplot()
 ax.set_aspect( 1 )
-ax.set_xlim(-lim, lim)
-ax.set_ylim(-lim, lim)
+#ax.set_xlim(-lim, lim)
+#ax.set_ylim(-lim, lim)
 
 for i in range(3, -1, -1):
     ax.add_artist(plt.Circle((0, 0), 
@@ -25,3 +26,44 @@ for i in range(3, -1, -1):
                              linestyle = "--",
                              alpha=0.2))
  
+   
+    
+""" Adding some streamplot stuff """
+# 1D arrays
+x = np.arange(-5,5,0.1)
+y = np.arange(-5,5,0.1)
+  
+# Meshgrid
+X,Y = np.meshgrid(x,y)
+  
+# Assign vector directions
+Ex = (X + 1)/((X+1)**2 + Y**2) - (X - 1)/((X-1)**2 + Y**2)
+Ey = Y/((X+1)**2 + Y**2) - Y/((X-1)**2 + Y**2)
+
+ax.streamplot(X,Y,Ex,Ey, density=1.4, linewidth=None, color="black")
+
+
+
+""" Adding some quiver stuff """
+x,y = np.meshgrid(np.linspace(-5,5,10),np.linspace(-5,5,10))
+
+u = x/np.sqrt(x**2 + y**2)
+v = y/np.sqrt(x**2 + y**2)
+plt.quiver(x,y,u,v, color="red")
+
+
+print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
