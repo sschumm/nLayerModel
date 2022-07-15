a05e526414b9bc932e38d4456f35482db77addf7
sschumm <sven.schumm7@gmail.com>
2022-07-15 14:17:37 +0200

    Did some more testing on somemath.py
    - Added some Errors raises for wrong dimensions & stuff

--- Moduls_Tests/somemath.py
+++ Moduls_Tests/somemath.py
@@ -51,7 +51,14 @@ def dfb(r, p):
 # -----------------------
 
 def solution_for_A(r, p, aj, bj, fa, fb):
-    return aj * fa(r, p) + bj * fb(r, p)  
+    # Note: Huge bug potential when trying to use arrays for r and for aj/bj
+    if type(aj) is not type(bj):
+        raise Exception("(sschumm): aj and bj have to be of the same datatype")
+    if type(aj) is np.ndarray and type(r) is np.ndarray:
+        raise Exception("(sschumm): this function is only designed to accept multiple r's or multiple solutions aj and bj")
+    
+    return np.add(aj * fa(r, p), bj * fb(r, p))
+    
 
 
 def Ar_no_k(r, p, aj, bj):
@@ -65,11 +72,11 @@ def dAr_no_k(r, p, aj, bj):
 # --- Testbench ------------------------------------------------------
 
 
-theta = np.arange(0, 2 * np.pi, np.pi/4, dtype=np.longdouble)
-r = np.linspace(1, 8, theta.shape[0], dtype=np.longdouble)
+theta = 2 # np.arange(0, 2 * np.pi, np.pi/4, dtype=np.longdouble)
+r = 3 # np.linspace(1, 8, theta.shape[0], dtype=np.longdouble)
 p = 2
-aj = 4
-bj = 3
+aj = 3 # np.linspace(1,5, 5) # 4
+bj = 3  #np.linspace(5,1, 5) # 4
 
 print("r0_to_xy(r, theta): \n", r0_to_xy(r, theta), "\n")
 #
@@ -78,7 +85,7 @@ print("fb(r, p): \n", fb(r, p), "\n")
 print("dfa(r, p): \n", dfa(r, p), "\n")
 print("dfb(r, p): \n", dfb(r, p), "\n")
 #
-print("solution_for_A(r, p, aj, bj, fa, fb): \n", solution_for_A(r, p, aj, bj, fa, fb), "\n")
+#print("solution_for_A(r, p, aj, bj, fa, fb): \n", solution_for_A(r, p, aj, bj, fa, fb), "\n")
 print("Ar_no_k(r, p, aj, bj): \n", Ar_no_k(r, p, aj, bj), "\n")
 print("dAr_no_k(r, p, aj, bj): \n", dAr_no_k(r, p, aj, bj), "\n")
 
