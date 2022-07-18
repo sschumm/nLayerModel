9b3fbdbc9a685b7a65834bdc8a3ce57afbf16aa1
sschumm <sven.schumm7@gmail.com>
2022-07-18 21:48:42 +0200

    Tried some more stuff, got a plan for tomorrow

--- Moduls_Tests/even_spacing.py
+++ Moduls_Tests/even_spacing.py
@@ -8,11 +8,46 @@ Created on Mon Jul 18 18:08:06 2022
 import numpy as np
 import matplotlib.pyplot as plt 
 
+np.set_printoptions(suppress=True, linewidth=200, precision=2)
 
 lim = 7.
-c = np.linspace(-lim, lim, 16)
+detail = 7
+c = np.linspace(-lim, lim, detail)
 X, Y = np.meshgrid(c, c)
 
+getR = lambda x, y: np.sqrt(x**2, y**2)
+get0 = lambda x, y: np.arctan2(y, x)
+
+R = getR(X, Y)
+THETA = get0(X, Y)
+
+#%%
+R1, T1 = np.meshgrid(np.linspace(0.01,lim,detail), np.linspace(0, 2*np.pi, detail*2))
+
+getX = lambda r, t: r * np.cos(t)
+getY = lambda r, t: r * np.sin(t)
+
+X1 = getX(R1, T1)
+Y1 = getY(R1, T1)
+
+
+"""
+Try out:
+    X, Y meshgrid
+    get r
+    get 0
+    
+    create new r matrix with linspace -> np.tile(arr, (Theta.shape[0], 1))
+    compute Br, B0
+    compute X, Y back
+    
+
+
+"""
+
+
+
+#%%
 fu = lambda x, y: x**2 + y
 fv = lambda x, y: - x + y**2
 
