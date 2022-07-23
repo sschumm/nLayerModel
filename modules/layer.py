bd5555092078b6ea781502a443c5a5c582a3e1cc
sschumm <sven.schumm7@gmail.com>
2022-07-23 14:41:28 +0200

    Started rebuild layer.py

---
+++ modules/layer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 23 14:34:28 2022
+
+@author: svens
+"""
+
+# import numpy as np
+
+from scipy.constants import mu_0
+
+
+
+class Layer():
+    
+    
+    def __init__(self, r, mu_r):
+        
+        self.r = r
+        self.mu = mu_0 * mu_r
+        self.mu_inv = 1 / self.mu
+        
+        self.idx = None
+        
+        
+    def set_index(self, index):
+        self.idx = index
+        
+    
