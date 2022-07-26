4f8e9af4d6cd5d91cb350fde39dfaff59b4c6139
sschumm <sven.schumm7@gmail.com>
2022-07-26 10:07:32 +0200

    Finished the 2nd analytical comparison

--- analytics/three_regions.py
+++ analytics/three_regions.py
@@ -12,15 +12,21 @@ from scipy.constants import mu_0
 # -- iron -- | | -- air -- |k| -- air --
 
 
-a_1 = lambda p, r1, r2, K, mu_1: (mu_0*mu_1 * K) / ((mu_0+mu_1)*p) * r2**(-p+1)
+a_1 = lambda p, r1, r2, K, mu_1: (mu_0*mu_1) / (mu_0+mu_1) * (K/p) * r2**(-p+1)
 b_1 = 0.
 a_2 = lambda p, r1, r2, K, mu_1: (mu_0*K) / (2*p) * r2**(-p+1)
 b_2 = lambda p, r1, r2, K, mu_1: (a_1(p, r1, r2, K, mu_1) - a_2(p, r1, r2, K, mu_1)) * r1**(2*p)
 a_3 = 0.
 b_3 = lambda p, r1, r2, K, mu_1: a_2(p, r1, r2, K, mu_1) * r2**(2*p) + b_2(p, r1, r2, K, mu_1)
+# =============================================================================
+# b_2 = lambda p, r1, r2, K, mu_1: (K/p) * ((mu_0*mu_1)/(mu_0+mu_1) - (mu_0/2)) * r1**(2*p) * r2**(-p+1)
+# a_3 = 0.
+# b_3 = lambda p, r1, r2, K, mu_1: (K/p) * ((mu_0/2) * r2**(p+1) + ((mu_0*mu_1)/(mu_0+mu_1) - (mu_0/2)) * r1**(2*p) * r2**(-p+1))
+# =============================================================================
 
 
-def analytic_solution(p, r1, r2, K, mu_1):
+def analytic_solution(p, r1, r2, K, mu_r):
+    mu_1 = mu_r * mu_0
     solution = [a_1(p, r1, r2, K, mu_1), 
                 b_1, 
                 a_2(p, r1, r2, K, mu_1), 
