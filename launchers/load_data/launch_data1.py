2df2de1535dc960e58249f60430aff9cea137921
sschumm <sven.schumm7@gmail.com>
2022-08-03 11:38:12 +0200

    Tried improving the way of defining layers via json-files

--- launchers/load_data/launch_data1.py
+++ launchers/load_data/launch_data1.py
@@ -15,40 +15,38 @@ from data.load import info, load_json, data
 # dic = load_json("kostopoulos")
 # info("kostopoulos")
 
-print(data("kostopoulos"))
+data = data("kostopoulos")
+print(data)
 
 # -------- create model --------
-# =============================================================================
-# model = Model(p)
-# 
-# model.add_layer(AirLayer(r=r_ri))
-# model.add_layer(MagneticLayer(r=r_ro, mu_r=m_rr))
-# model.add_layer(CurrentLoading(K=K_r, r=r_ro+wd, alpha=pi * 0.0))
-# model.add_layer(CurrentLoading(K=K_s, r=r_si-wd, alpha=pi * 0.5))
-# model.add_layer(AirLayer(r_si))
-# model.add_layer(MagneticLayer(r_so, mu_r=m_rs))
-# 
-# model.build()
-# model.solve()
-# model.total_torque()
-# 
-# # -------- output -------- 
-# print("x =", model.x, "\n")
-# print("M =", model.M, "\n")
-# 
-# rM_plot = RadialMultiPlot(model)
-# rM_plot.set_Br_details(title="Br")
-# rM_plot.set_Ht_details(title="Ht")
-# # rM_plot.multiplot(angle=90)
-# 
-# p_plot = PlanePlot(model)
-# # p_plot.contour(dr=400, dt=200)
-# # p_plot.streamplot(dr=400, dt=200)
-# 
-# pd_plot = PlaneDoublePlot(model)
-# # pd_plot.plot_BandA(dr=400, dt=200)
-# 
-# 
-# 
-# 
-# =============================================================================
+model = Model(data["p"])
+
+
+# model.add_layer(CurrentLoading(K=data["K_r"], r=data["layers"]["radii"][0], alpha=pi * 0.0))
+model.add_layer(CurrentLoading(K=data["K_s"], r=data["layers"]["radii"][1], alpha=pi * 0.5))
+model.add_layer(AirLayer(r=data["layers"]["radii"][2]))
+
+
+model.build()
+model.solve()
+model.total_torque()
+
+# -------- output -------- 
+print("x =", model.x, "\n")
+print("M =", model.M, "\n")
+
+rM_plot = RadialMultiPlot(model)
+rM_plot.set_Br_details(title="Br")
+rM_plot.set_Ht_details(title="Ht")
+# rM_plot.multiplot(angle=90)
+
+p_plot = PlanePlot(model)
+p_plot.contour(dr=400, dt=200)
+# p_plot.streamplot(dr=400, dt=200)
+
+pd_plot = PlaneDoublePlot(model)
+# pd_plot.plot_BandA(dr=400, dt=200)
+
+
+
+
