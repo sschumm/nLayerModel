43f1b3eca74581a21d042721972446ccbc5aa0d1
sschumm <sven.schumm7@gmail.com>
2022-08-18 11:11:42 +0200

    Updated robin_gen1.py to apply new r_f.

--- launchers/load_data/robin_gen1.py
+++ launchers/load_data/robin_gen1.py
@@ -20,27 +20,23 @@ p = 39
 q = 2
 a  = 156
 mu_r = 1e6
-sigma = 0.5 # sigma=0 -> breadth=0, sigma=1 -> breadth=pi
+sigma = 0.9 # sigma=0 -> breadth=0, sigma=1 -> breadth=pi
 Nf = 63
 Ns = 89
 
 
 # [A]
-
-# --!--
 I_f = 416.897
-I_f = 2.8644 * 1e4
-# ----
-
 I_a = 6.1654 * 1e3
 
 
 # [m]
-rdelta = 3.0661
+h_p = 0.0125 # <- hp_layer_f,   hp -> 0.0481
 lfe = 1.1173  # l_i = 1.2003 # L = 1.2023 
 rri = 2.9532 
 rro = 3.0475 
-r_f = rdelta
+r_f = rro + h_p*0.5
+rdelta = 3.0661
 r_a = 3.0847
 rsi = 3.0847
 rso = 3.2543
@@ -51,14 +47,8 @@ k_d = kd(m=m, q=q)
 k_b = kb(n=1, sigma=sigma)
 
 # [A/m]
-
-# --!--
-A_r = K(m=1, I=I_f, d=2*r_f, N=2*p*Nf)
-A_r = K(m=1, I=I_f, d=2*r_f, N=2*2*p*Nf) # 2 layer?
-A_r = K(m=1, I=I_f, d=2*r_f, N=Nf)
-A_r = K(m=1, I=I_f, d=2*r_f, N=2*Nf)
-# ----
-
+# A_r = K(m=1, I=I_f, d=2*r_f, N=2*p*Nf) # 1 layer field winding
+A_r = K(m=1, I=I_f, d=2*r_f, N=2*2*p*Nf) # 2 layer field winding
 A_s = K(m=m, I=I_a, d=2*r_a, N=Ns) # given: A_s=1.6987*1e5 [A/m]
 
 A_r_amplitude = np.sqrt(2) * A_r
@@ -70,17 +60,15 @@ n_syn = 8.33   # [rpm]
 # [rad/s]
 w_syn = 0.8723 
 gamma = 2.9044
-alpha_f = pi * 0.5 # gamma * 0.5 
+alpha_f = pi * 0.5
 alpha_a = pi * 0.0
-
-# rdelta = 3.0661, 
 
 
 #%% -------------------- model setup --------------------
 
 model = Model(p=p, l=lfe)
 
-# Air -| |- Iron -| |- Air -|k|- Air -|k|- Air -||- Iron -||- Env 
+# Air -| |- Iron -| |- Air -|k|- Air -|k|s- Iron -||- Env 
 
 model.add_layer(AirLayer(r=rri))
 model.add_layer(MagneticLayer(r=rro, 
@@ -95,7 +83,7 @@ model.add_layer(CurrentLoading(K=A_s_amplitude,
                                alpha=alpha_a,
                                mu_r=1.
                                ))
-model.add_layer(AirLayer(r=rsi))
+# model.add_layer(AirLayer(r=rsi))
 model.add_layer(MagneticLayer(r=rso, 
                               mu_r=mu_r))
 
