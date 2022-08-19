f770b7c9c34e23322175390846df4f616f00d8c7
sschumm <sven.schumm7@gmail.com>
2022-08-19 09:01:35 +0200

    Updated computation of A_r_amplitude in song_gen1/2.

--- launchers/data_study/song_gen2.py
+++ launchers/data_study/song_gen2.py
@@ -20,7 +20,6 @@ p = 12
 q = 4
 a  = 1
 mu_r = 1e6
-sigma = 0.5
 Nf_pp = 3220 # =/= gen1
 Na_ps = 22
 
@@ -32,24 +31,30 @@ I_a = 760
 
 # [m]
 lfe = 0.8 
-rri = 1.55 # 1.49 #1.55  # =/= gen1
+rri = 1.55 # =/= gen1
 rro = 1.632 # =/= gen1
 r_f = 1.736 # =/= gen1
 r_a = 1.92
 rsi = 1.974
 rso = 2.05
 
+# Rotor Winding Factor
+# breadth factor, params taken from fig. 2
+coil_width = 0.12 # [m]
+coil_side_distance = 0.18 # [m]
+sigma_r = coil_width / (coil_width+coil_side_distance)
+kr_b = kb(n=1, sigma=sigma_r)
 
-# [-]
-k_d = kd(m=m, q=q) 
-k_b = kb(n=1, sigma=sigma)
+# Stator Winding Factor
+# zone factor == given winding factor: 0.9577
+ks_d = kd(m=m, q=q)
 
 # [A/m]
 A_r = K(m=1, I=I_f, d=2*r_f, N=2*p*Nf_pp)
 A_s = K(m=m, I=I_a, d=2*r_a, N=q*p*Na_ps) # given: A_s = 400 [kA/m]
 
-A_r_amplitude = np.sqrt(2) * A_r
-A_s_amplitude = np.sqrt(2) * A_s * k_d * k_b
+A_r_amplitude = np.sqrt(2) * A_r * kr_b
+A_s_amplitude = np.sqrt(2) * A_s * ks_d 
 
 
 n_rated = 10   # [rpm]
@@ -98,13 +103,13 @@ print(f"{A_s_amplitude = } [A/m]")
 
 # [MW]
 P_out_target = 10 
-P_out_model = w_rated * model.M / 1e6
+P_out_model = w_rated * model.Mneg / 1e6
 
 print(f"{P_out_target = } [MW]")
 print(f"{P_out_model  = } [MW]")
 
 # [MN]
-Torque = model.M/1e6
+Torque = model.Mneg/1e6
 print(f"{Torque = } [MN]")
 
 # [T]
