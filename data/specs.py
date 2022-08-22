a508e578dd5dd07ee53a6ef01ad405d6d44d6c55
sschumm <sven.schumm7@gmail.com>
2022-08-22 18:36:31 +0200

    Added specs.py

---
+++ data/specs.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from scipy.constants import pi
+
+class Generator():
+
+    Pel_out = 7 * 1e6  # == 7 [MW]
+    n_syn = 8.33       # [rpm]
+    w_syn = 0.8723     # [rad/s]        
+    U_LL_N = 690       # [V] - nominal line to line voltage
+    delta_mag = 0.04   # == 40 [mm]
+    mu_r_yoke = 1e5    # ferro magnetic
+    mu_r_core = 1      # G-10 CR (Fiberglass Epoxy) or Stainless Steel
+    
+    
+class FieldWinding():
+    
+    T_HTS = 30 # [K] - LNe - Liquid Neon
+    J_e = 20/1.7 # ~ 11.8 [A/mm2] - engineering current density
+    
+    
+class StatorWinding_Cu():
+    
+    # T_HTS = 77 # [K] - LN2 - Liquid Nitrogen
+    J_e = 3.8 # [A/mm2] - current density
+    C_e = 37.71 # [kVA min/m3] - Essons Number - electromagnetic utilization
+    d2so_L = 22.28 # [m3]
+    V_ges = pi/4 * d2so_L
+
+    
+class StatorWinding_77K():
+    
+    T_HTS = 77 # [K] - LN2 - Liquid Nitrogen
+    J_e = 20/1.7 # ~ 11.8 [A/mm2] - engineering current density
+    C_e = 0.5 * 117 # [kVA min/m3] - Essons Number - electromagnetic utilization
+    d2so_L = 11.28 # [m3]
+    
+    
+class StatorWinding_30K():
+    
+    T_HTS = 30 # [K] - LNe - Liquid Neon
+    J_e = 270/1.7 # ~ 158.8 [A/mm2] - engineering current density
+    C_e = 0.5 * 1575.9 # [kVA min/m3] - Essons Number - electromagnetic utilization
+    d2so_L = 1 # [m3]
