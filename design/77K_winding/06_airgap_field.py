1501908b57cac9ab7e00c9b221b17a416551488c
sschumm <sven.schumm7@gmail.com>
2022-10-04 11:28:48 +0200

    Updated 06_airgap_field.py
    - Added part to find the current loadings such that the critical ...
      ... field is not surpassed
    - NEXT STEP: put this in some kind of loop to achieve Pel_goal

--- design/77K_winding/06_airgap_field.py
+++ design/77K_winding/06_airgap_field.py
@@ -8,7 +8,9 @@ from data import StatorWinding_77K as sw
 from design import Main_Params, Main_Dims, create_n_Layer_model
 from design import get_L_TPL2100 as L
 
+K_s, K_r = 0, 0
 Pel_goal = gn.Pel_out * 1
+tolerance_lower, tolerance_upper = 0.95, 1.1
 A_tape = (12*0.22) * 1e-6 # [m**2]
 Ic_0 = 300 # [A]
 r_so, l_e, dims = None, None, None
@@ -41,8 +43,11 @@ update_dimensions()
 init_params = Main_Params(p, l_e, r_so, r_si, k_fill_r=0.5, k_fill_s=0.5, B_yoke_max=6)
 init_dims = Main_Dims(r_so, r_si, r_sA, r_rF, r_ro, r_ri)
 
-K_s_init = init_params.k_fill_s * sw.J_e * h_wndg_s * 0.8    # reduce given vals by factor
-K_r_init = init_params.k_fill_r * fw.J_e * h_wndg_r * 0.3    # reduce given vals by factor 
+J_e_s_init = sw.J_e * 0.8 # reduce given vals by factor
+J_e_r_init = fw.J_e * 0.3 # reduce given vals by factor 
+
+K_s_init = init_params.k_fill_s * h_wndg_s * J_e_s_init
+K_r_init = init_params.k_fill_r * h_wndg_r * J_e_r_init
 
 """ --- find model with static K_s and K_r --- """
 if True:
@@ -59,7 +64,7 @@ if True:
         iter_static_P_history.append(res.P)
         
         # --- adapt stator current loading ---
-        if res.P < 1.1 * Pel_goal and res.P > 0.9 * Pel_goal:
+        if res.P < tolerance_upper * Pel_goal and res.P > tolerance_lower * Pel_goal:
             res.show(f"{iter_idx_static = }")
             break
         elif res.P < Pel_goal:
@@ -93,7 +98,7 @@ if True:
         iter_static_P_history.append(res.P)
         
         # --- adapt rotor current loading ---
-        if res.P < 1.1 * Pel_goal and res.P > 0.9 * Pel_goal:
+        if res.P < tolerance_upper * Pel_goal and res.P > tolerance_lower * Pel_goal:
             res.show(f"{iter_idx_static = }")
             break
         elif res.P < Pel_goal:
@@ -135,7 +140,57 @@ if True:
     
     # res.show("model with adjusted yokes")
     # plt.fluxplot(dr,dt,lvls=10)
+    ...
 
+K_s_static = K_s
+K_r_static = K_r
 
 """ --- find model with B dependent engineering current density --- """
-...
+if True:
+    # --- find densities and loadings for B_airgap from lift factor ---
+    J_e_s = L(sw.T_HTS, res.B_airgap, 30) * Ic_0/(A_tape*1.7)
+    J_e_r = L(fw.T_HTS, res.B_airgap, 30) * Ic_0/(A_tape*1.7)    
+    K_s_lift = init_params.k_fill_s * J_e_s * h_wndg_s
+    K_r_lift = init_params.k_fill_r * J_e_r * h_wndg_r
+    
+    # --- print differences between init and lift densities and loadings ---
+    if False:
+        print("")
+        print(f"{J_e_s/1e6      = }, \n{J_e_r/1e6      = }")
+        print(f"{J_e_s_init/1e6 = }, \n{J_e_r_init/1e6 = }")
+        print(f"{K_s_lift/1e6   = }, \n{K_r_lift/1e6   = }")
+        print(f"{K_s_static/1e6 = }, \n{K_r_static/1e6 = }")
+    
+    iter_lift_K_s_history = [K_s_static] * 9 + [K_s_lift]
+    iter_lift_K_r_history = [K_r_static] * 9 + [K_r_lift]
+    
+    for iter_idx_lift in range(100):
+        
+        div = 10
+        K_s = sum(iter_lift_K_s_history[-div:]) / div
+        K_r = sum(iter_lift_K_r_history[-div:]) / div
+        
+        # print(K_s)
+        
+        mdl, plt, res = create_n_Layer_model(dims=Main_Dims(r_so, r_si, r_sA, r_rF, r_ro, r_ri), 
+                                              p   =init_params.p, 
+                                              l   =init_params.l_e,
+                                              Ks  =K_s,
+                                              Kr  =K_r)
+        
+        J_e_s = L(sw.T_HTS, res.B_airgap, 30) * Ic_0/(A_tape*1.7)
+        J_e_r = L(fw.T_HTS, res.B_airgap, 30) * Ic_0/(A_tape*1.7)    
+        K_s_lift = init_params.k_fill_s * J_e_s * h_wndg_s
+        K_r_lift = init_params.k_fill_r * J_e_r * h_wndg_r
+        
+        iter_lift_K_s_history.append(K_s_lift)
+        iter_lift_K_r_history.append(K_r_lift)
+        
+    # --- plot convergence ---
+    if False:    
+        pyplt.figure(dpi=1000)
+        pyplt.plot([i for i in range(len(iter_lift_K_s_history))], iter_lift_K_s_history)
+        pyplt.plot([i for i in range(len(iter_lift_K_r_history))], iter_lift_K_r_history)
+        
+        
+    ...
