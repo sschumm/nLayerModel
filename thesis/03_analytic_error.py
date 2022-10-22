0a3bbd0ca7a77413c5afdd0ff2918d76e35e26e3
sschumm <sven.schumm7@gmail.com>
2022-10-22 20:18:04 +0200

    Added more data to 03_analytic_error.py

--- thesis/03_analytic_error.py
+++ thesis/03_analytic_error.py
@@ -7,7 +7,7 @@ from analytics.two_regions import analytic_solution
 np.set_printoptions(suppress=True, linewidth=250, precision=40)
 
 
-def f(p, r, K, atol, rtol):
+def f(p, r, K, atol, rtol, only_b2=True):
     model = Model(p)
     model.add_layer(CurrentLoading(K, r, mu_r=1))
     model.build()
@@ -20,28 +20,55 @@ def f(p, r, K, atol, rtol):
     
     close = np.allclose(x_numeric, x_analytic, rtol=rtol, atol=atol)
     
-    #return close, x_numeric, x_analytic
-    return (b2_n, b2_a)
-
-
-#%% 
+    if only_b2:
+        return b2_n, b2_a
+    else:
+        return close, x_numeric, x_analytic
 
 rtol = 0
 atol = 1e-15
 
+#%% static print
+
+f(p=3, r=2.0, K=1e6, rtol=rtol, atol=atol, only_b2=False)
+
+#%% static work
+
+r = 2.0
+p = 100
+c, x_num, x_an = f(p=p, r=r, K=1e6, rtol=rtol, atol=atol, only_b2=False)
+
+A_jr_num = np.array([r**p, r**-p]*2) * x_num
+A_jr_an  = np.array([r**p, r**-p]*2) * x_an
+
+print(A_jr_num)
+print(A_jr_an)
+
+
+#%% only b2 plot
 
 x = np.arange(1,100)
 y1 = list()
 y2 = list()
+y3 = list()
+y4 = list()
 
 for p in x:
-    n, a = f(p=p, r=2., K=1e6, rtol=rtol, atol=atol)
+    n, a = f(p=p, r=2.0, K=1e6, rtol=rtol, atol=atol)
     y1.append(np.abs(n - a))
 
 for p in x:
     n, a = f(p=p, r=1.5, K=1e6, rtol=rtol, atol=atol)
     y2.append(np.abs(n - a))
+    
+for p in x:
+    n, a = f(p=p, r=1.1, K=1e6, rtol=rtol, atol=atol)
+    y3.append(np.abs(n - a))
 
+for p in x:
+    n, a = f(p=p, r=0.8, K=1e6, rtol=rtol, atol=atol)
+    y4.append(np.abs(n - a))
+    
 fig = plt.figure(dpi=300)
 ax = plt.subplot()
 # ax.plot(x, y)
@@ -49,4 +76,72 @@ ax = plt.subplot()
 markersize = 5
 ax.scatter(x, y1, s=markersize)
 ax.scatter(x, y2, s=markersize)
+ax.scatter(x, y3, s=markersize)
+ax.scatter(x, y4, s=markersize)
+ax.set_yscale("log")
+
+#%%
+
+x = np.arange(1,100)
+y1 = list()
+y2 = list()
+y3 = list()
+y4 = list()
+
+for p in x:
+    r=2.0
+    c, x_num, x_an = f(p=p, r=r, K=1e6, rtol=rtol, atol=atol, only_b2=False)
+
+    A_jr_num = np.array([r**p, r**-p]*2) * x_num
+    A_jr_an  = np.array([r**p, r**-p]*2) * x_an
+    
+    n = A_jr_num[3]
+    a = A_jr_an[3]
+    
+    y1.append(np.abs(n - a))
+    
+for p in x:
+    r=1.5
+    c, x_num, x_an = f(p=p, r=r, K=1e6, rtol=rtol, atol=atol, only_b2=False)
+
+    A_jr_num = np.array([r**p, r**-p]*2) * x_num
+    A_jr_an  = np.array([r**p, r**-p]*2) * x_an
+    
+    n = A_jr_num[3]
+    a = A_jr_an[3]
+    
+    y2.append(np.abs(n - a))
+    
+for p in x:
+    r=1.1
+    c, x_num, x_an = f(p=p, r=r, K=1e6, rtol=rtol, atol=atol, only_b2=False)
+
+    A_jr_num = np.array([r**p, r**-p]*2) * x_num
+    A_jr_an  = np.array([r**p, r**-p]*2) * x_an
+    
+    n = A_jr_num[3]
+    a = A_jr_an[3]
+    y3.append(np.abs(n - a))
+
+for p in x:
+    r=0.8
+    c, x_num, x_an = f(p=p, r=r, K=1e6, rtol=rtol, atol=atol, only_b2=False)
+
+    A_jr_num = np.array([r**p, r**-p]*2) * x_num
+    A_jr_an  = np.array([r**p, r**-p]*2) * x_an
+    
+    n = A_jr_num[3]
+    a = A_jr_an[3]
+    y4.append(np.abs(n - a))
+    
+fig = plt.figure(dpi=300)
+ax = plt.subplot()
+plt.grid(which="both")
+# ax.plot(x, y)
+# ax.semilogy(x, y)
+markersize = 5
+ax.scatter(x, y1, s=markersize)
+ax.scatter(x, y2, s=markersize)
+ax.scatter(x, y3, s=markersize)
+ax.scatter(x, y4, s=markersize)
 ax.set_yscale("log")
