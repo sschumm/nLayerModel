08bd657cd75ec104dbd43bdda85217fd3dd2fc9a
sschumm <sven.schumm7@gmail.com>
2022-10-30 08:14:00 +0100

    Added save_params to 01_basics.py
    - changed some parameters
    - used deprecated design script to validate this one
    - switched varying of stator and rotor

--- design/77K/01_basics.py
+++ design/77K/01_basics.py
@@ -9,24 +9,25 @@ from analytics import yoke_height
 from data import Generator as gn
 from data import FieldWinding as fw
 from data import StatorWinding_77K as sw
+from data.export import save_params
 
 from design import n7_Model
 
 # --- init generator with main parameter ---
-p = 18
-l_e = 0.3
+p = 25
+l_e = 0.2
 r_so = sw.r_so(sw, l_e)
 
 generator = n7_Model(p, l_e, r_so, 
                      gn=gn, fw=fw, sw=sw,
                      k_fill_s=0.3,
-                     k_fill_r=0.3,
+                     k_fill_r=0.2,
                      B_yoke_max=6.0)
 
 
 # --- init generator dimension via yoke and winding heights ---
 h_yoke_s, h_yoke_r = 0.02 * r_so, 0.02 * r_so
-h_wndg_s, h_wndg_r = 0.5 * gn.delta_mag, 0.5 * gn.delta_mag
+h_wndg_s, h_wndg_r = [factor * (gn.h_pole_frame*2) for factor in [1.5, 1.5]]
 
 generator.init_dimensions(h_yoke_s, h_yoke_r, h_wndg_s, h_wndg_r)
 
@@ -35,15 +36,15 @@ generator.init_dimensions(h_yoke_s, h_yoke_r, h_wndg_s, h_wndg_r)
 K_s = generator.k_fill_s * generator.J_e_s * generator.h_wndg_s
 K_r = generator.k_fill_r * generator.J_e_r * generator.h_wndg_r
 
-generator.update_model_by_K(K_s = K_s, K_r = K_r)
+generator.update_model_by_K(K_s = K_s, K_r = K_r, ks_d=0.9577)
 generator.show_results("Results for the initial model")
-if 0:generator.fast_flux()
+# generator.fast_flux()
 
 
 #%% --- apply the lift factor to be self-consistent ---
 generator.apply_lift_factor(verbose = False)
 generator.show_results("Results for initial dimensions with applied lift factor")
-if 0: generator.fast_flux()
+# generator.fast_flux()
 
 #%% --- adapt yokes ---
 # --- adapt stator yoke ---
@@ -57,15 +58,57 @@ h_yoke_r = yoke_height(generator.p, generator.dims.r_ro,
 generator.update_dimensions(h_yoke_s=h_yoke_s, h_yoke_r=h_yoke_r)
 generator.apply_lift_factor(verbose = False)
 generator.show_results("Results with adapted yokes")
-if 0: generator.fast_flux()
+generator.fast_flux()
+
+#%% --- vary h_wndg_s ---
+if 1:
+    increasing_h_by_factor=0.01
+    iter_P_h_wndg_s, iter_h_wndg_s = [generator.P], [generator.h_wndg_s]
+    
+    for idx_h_wndg_s in range(50):
+        h_wndg_s = iter_h_wndg_s[-1] + iter_h_wndg_s[0] * increasing_h_by_factor
+        if h_wndg_s <= 0 or h_wndg_s >= generator.dims.r_ro/2:
+            break
+        generator.update_dimensions(h_wndg_s=h_wndg_s)
+        generator.apply_lift_factor()
+        
+        iter_h_wndg_s.append(generator.h_wndg_s)
+        iter_P_h_wndg_s.append(generator.P)
+        
+    
+    fig, axs = pyplot.subplots(1)
+    fig.tight_layout() 
+    fig.dpi=500
+    fig.set_figheight(4)
+    fig.set_figwidth(5)
+    
+    ax1 = axs
+    ax1.grid()
+    ax1.plot(iter_h_wndg_s, [i*1e-6 for i in iter_P_h_wndg_s])
+    ax1.set_xlabel("h_wndg_s [m]")
+    ax1.set_ylabel('P [MW]', color="b")
+
+#%% --- select h_wndg_s ---
+if 1:
+    h_wndg_s = 0.029
+    generator.update_dimensions(h_wndg_s=h_wndg_s)
+    generator.apply_lift_factor(verbose=True)
+    h_yoke_s = yoke_height(generator.p, generator.dims.r_si, 
+                           generator.B_s, generator.B_yoke_max)
+    generator.update_dimensions(h_yoke_s=h_yoke_s)
+    generator.apply_lift_factor()
+    generator.show_results("Results for adapted h_wndg_s")
+    # generator.fast_flux()
 
 #%% --- vary h_wndg_r ---
 if 1:
-    increasing_h_by_factor=0.2
-    iter_P_h_wndg_r, iter_h_wndg_r = [generator.P], [generator.h_wndg_r]
+    increasing_h_by_factor=0.05
+    iter_P_h_wndg_r, iter_h_wndg_r = [generator.P], [generator.h_wndg_r*0.8]
     
     for idx_h_wndg_r in range(100):
         h_wndg_r = iter_h_wndg_r[-1] + iter_h_wndg_r[0] * increasing_h_by_factor
+        if h_wndg_r <= 0 or h_wndg_r >= generator.dims.r_ro/2:
+            break
         generator.update_dimensions(h_wndg_r=h_wndg_r)
         generator.apply_lift_factor()
         
@@ -87,58 +130,23 @@ if 1:
 
 #%% --- select h_wndg_r ---
 if 1:
-    h_wndg_r = 0.1
+    h_wndg_r = 0.03
     generator.update_dimensions(h_wndg_r=h_wndg_r)
     generator.apply_lift_factor(verbose=True)
+    h_yoke_r = yoke_height(generator.p, generator.dims.r_ro, 
+                           generator.B_r, generator.B_yoke_max)
+    generator.update_dimensions(h_yoke_r=h_yoke_r)
+    generator.apply_lift_factor()
     generator.show_results("Results for adapted h_wndg_r")
-    if 0: generator.fast_flux()
-
-
-#%% --- vary h_wndg_s ---
-if 1:
-    # --- interating ---
-    increasing_h_by_factor=0.1
-    iter_P_h_wndg_s, iter_h_wndg_s = [generator.P], [generator.h_wndg_s]
-    
-    for idx_h_wndg_s in range(50):
-        h_wndg_s = iter_h_wndg_s[-1] + iter_h_wndg_s[0] * increasing_h_by_factor
-        generator.update_dimensions(h_wndg_s=h_wndg_s)
-        generator.apply_lift_factor()
-        
-        iter_h_wndg_s.append(h_wndg_s)
-        iter_P_h_wndg_s.append(generator.P)
-        
-    
-    fig, axs = pyplot.subplots(1)
-    fig.tight_layout() 
-    fig.dpi=500
-    fig.set_figheight(4)
-    fig.set_figwidth(5)
-    
-    ax1 = axs
-    ax1.grid()
-    ax1.plot(iter_h_wndg_s, [i*1e-6 for i in iter_P_h_wndg_s])
-    ax1.set_xlabel("h_wndg_s [m]")
-    ax1.set_ylabel('P [MW]', color="b")
-
-#%% --- select h_wndg_s ---
-if 1:
-    h_wndg_s = 0.05 
-    generator.update_dimensions(h_wndg_s=h_wndg_s)
-    generator.apply_lift_factor(verbose=True)
-    generator.show_results("Results for adapted h_wndg_s")
-    if 0: generator.fast_flux()
-        
-
-
+    # generator.fast_flux()
 
 #%% --- check coil dimensions ---
 generator.coil_shapes()
 generator.coil.show()
 
 
-
-
+#%%
+save_params("from_python", generator)
 
 
 
