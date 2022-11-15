5d300672618da3340296b052018b19ec082815bf
sschumm <sven.schumm7@gmail.com>
2022-11-15 14:12:54 +0100

    Added plots to 07_specs.py

--- thesis/07_specs.py
+++ thesis/07_specs.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+import tikzplotlib as tkz
 from design import get_L_TPL2100 as L
 
 P = 7000
@@ -40,3 +43,60 @@ print(f"{C_30_a = }")
 d2l_30 = P / (C_30_a * n)
 print(f"{d2l_30 = }")
 
+#%% visualize
+
+theta = 30 # [°]
+
+lst_T = [77, 30] # [T]
+lst_B = np.linspace(0,4,200) # [B]
+
+lst_J_e = []
+
+
+for T in lst_T:
+    
+    a = []
+    for B in lst_B:  
+        
+        J_e = L(T, B, theta) * J_base
+        a.append(J_e)
+
+    lst_J_e.append(a)
+    
+#%% plot 77K
+
+fig = plt.figure(dpi=300, figsize=(6,5))
+ax = plt.subplot()
+ax.set_xlabel("B / T")
+ax.set_ylabel("J_e / Amm2")
+ax.set_xlim(0, 3)
+ax.set_ylim(0, 25)
+# ax.set_xticks([i for i in range(0, 181, 20)])
+# ax.set_yticks([i for i in range(0, 2501, 250)])
+plt.grid()
+
+ax.plot(lst_B, lst_J_e[0], label=f"T = {lst_T[0]} / K")
+
+# ax.legend(loc="upper right")
+
+tkz.clean_figure()
+# tkz.save("aus_J_e_over_B_77K.tex")
+
+#%% plot 30K
+
+fig = plt.figure(dpi=300, figsize=(6,5))
+ax = plt.subplot()
+ax.set_xlabel("B / T")
+ax.set_ylabel("J_e / Amm2")
+ax.set_xlim(0, 3)
+ax.set_ylim(0, 400)
+# ax.set_xticks([i for i in range(0, 181, 20)])
+ax.set_yticks([i for i in range(0, 401, 80)])
+plt.grid()
+
+ax.plot(lst_B, lst_J_e[1], label=f"T = {lst_T[1]} / K")
+
+# ax.legend(loc="upper right")
+
+tkz.clean_figure()
+# tkz.save("aus_J_e_over_B_30K.tex")
