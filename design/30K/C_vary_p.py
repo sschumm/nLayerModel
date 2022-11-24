3fff7b0bdd4ce5543d58ad224b366898996f2ec2
sschumm <sven.schumm7@gmail.com>
2022-11-24 19:45:41 +0100

    30K/C_vary_p.py adapted coil_sizes

--- design/30K/C_vary_p.py
+++ design/30K/C_vary_p.py
@@ -19,7 +19,7 @@ from design import n7_Model
 
 #%% Ausgehend von Schlankheitsgrad
 
-d2l = 2 # m3
+d2l = 1 # m3
 l_d = 0.25 # -
 
 d_so = (d2l / l_d)**(1/3)
@@ -31,13 +31,14 @@ print(f"{l_e = }")
 
 #%% Schlankheitsgrad berechnen
 
-d2l = 2 # m3
+d2l = 1 # m3
 
-r_so = 1
+r_so = 0.8
 d_so = 2* r_so
 l_e = d2l / (d_so)**2
 l_d = l_e / d_so
 
+print(f"{l_e = }")
 print(f"{l_d = }")
 
 #%% Init generator Model
@@ -134,6 +135,18 @@ gen.apply_lift_factor()
 
 
 #%% Iterate over h_sw and h_rw with lift factor (and adapt yokes)
+job_init_time = time.time()
+
+p=8
+gen = n7_Model(p, l_e, r_so, gn=gn, fw=fw, sw=sw)
+gen.init_dimensions(h_yoke_s=h_pf, h_yoke_r=h_pf, 
+                    h_wndg_s=h_pf * 2.1, 
+                    h_wndg_r=h_pf * 2.1
+                    )
+gen.ks_d=0.866
+
+
+
 lst_stator = []
 h_sw0 = h_pf*2.1 #0.3 
 h_rw0 = h_pf*2.1 #0.3
@@ -142,19 +155,26 @@ for iter_stator in np.linspace(0,3,9):
     gen.k_fill_s = get_k_fill_s(r_si= gen.dims.r_si, h_sw= gen.h_wndg_s, p= gen.p)
     
     lst_rotor = []
-    for iter_rotor in np.linspace(0,3,20):
+    for iter_rotor in np.linspace(0,3,15):
         gen.update_dimensions(h_wndg_r= h_rw0 + h_rw0*iter_rotor)
         gen.k_fill_r = get_k_fill_r(r_ro= gen.dims.r_ro, h_rw= gen.h_wndg_r, p= gen.p)
-        gen.update_model_by_K(K_s= gen.k_fill_s * gen.J_e_s * gen.h_wndg_s, 
-                              K_r= gen.k_fill_r * gen.J_e_r * gen.h_wndg_r, 
-                              ks_d=0.866)
-        gen.apply_lift_factor()
+        # gen.update_model_by_K(K_s= gen.k_fill_s * gen.J_e_s * gen.h_wndg_s, 
+        #                       K_r= gen.k_fill_r * gen.J_e_r * gen.h_wndg_r, 
+        #                       ks_d=0.866)
+        # gen.apply_lift_factor()
+        gen.apply_coil_sizes_and_lift_factor()
         adapt_yokes()
-        gen.apply_lift_factor()
+        # gen.apply_lift_factor()
+        # gen.apply_coil_sizes_and_lift_factor()
         lst_rotor.append(copy.deepcopy(gen))
     lst_stator.append(lst_rotor)
+    
+job_finish_time = time.time()
+job_runtime = job_finish_time - job_init_time
+sys.stdout.write(f"\rJob finished with {gen.runs} solved models in {np.round(job_runtime,3)} seconds.")
+sys.stdout.flush()
 
-#%% Plot iteration of h_sw and h_rw with lift factor
+##%% Plot iteration of h_sw and h_rw with lift factor
 fig = plt.figure(dpi=300, figsize=(6,7))
 ax1 = plt.subplot()
 plt.grid()
@@ -165,20 +185,21 @@ for lst in lst_stator:
 ax1.set_xlabel("h_rw")
 ax1.set_ylabel("P")
 ax1.legend(loc="upper left")
+plt.title(f"Pole Pair Count p = {gen.p}")
 plt.show()
 
 #%% test configuration
 
-gen.update_dimensions(h_yoke_s=h_pf, h_yoke_r=h_pf, h_wndg_s= 0.047, h_wndg_r= 0.055)
+gen.update_dimensions(h_yoke_s=h_pf, h_yoke_r=h_pf, h_wndg_s= 0.047, h_wndg_r= 0.07)
 gen.k_fill_s = get_k_fill_s(r_si= gen.dims.r_si, h_sw= gen.h_wndg_s, p= gen.p)
 gen.k_fill_r = get_k_fill_r(r_ro= gen.dims.r_ro, h_rw= gen.h_wndg_r, p= gen.p)
 gen.update_model_by_K(K_s= gen.k_fill_s * gen.J_e_s * gen.h_wndg_s, 
                       K_r= gen.k_fill_r * gen.J_e_r * gen.h_wndg_r, 
                       ks_d=0.866)
-gen.apply_lift_factor()
+gen.apply_coil_sizes_and_lift_factor()
 adapt_yokes()
-gen.apply_lift_factor()
+gen.apply_coil_sizes_and_lift_factor()
 
 gen.show_results()
-# gen.fast_flux()
+gen.fast_flux()
 
