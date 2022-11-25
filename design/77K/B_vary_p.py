557d363dba039d151fe4c817cb65d0ee2442cfaf
sschumm <sven.schumm7@gmail.com>
2022-11-25 09:01:23 +0100

    Updated line search algorithm in 77K/B_vary_p.py

--- design/77K/B_vary_p.py
+++ design/77K/B_vary_p.py
@@ -27,70 +27,70 @@ l_e = 0.94
 
 
 #%% Prior Data Analysis
-# ============================== Initialize Iteration ==============================
-p = 30
-job_init_time = time.time()
-
-gen = n7_Model(p, l_e, r_so, gn=gn, fw=fw, sw=sw)
-h_pf = gn.h_pole_frame
-gen.init_dimensions(h_yoke_s=h_pf, h_yoke_r=h_pf, 
-                    h_wndg_s=h_pf * 2.1, h_wndg_r=h_pf * 2.1)
-gen.ks_d= 0.866
-
-# ============================== Stator Winding Iteration ==============================    
-lst_stator_iter = []
-h_sw0 = h_pf*2.1 #0.3 
-h_rw0 = h_pf*2.1 #0.3
-
-n_iters_s = 9
-n_times_s= 3
-n_iters_r = 12
-n_times_r=3 
-
-detail = 0.0001
-verbose = False
-
-for idx_stator, iter_stator in enumerate(np.linspace(0, n_times_s, n_iters_s)):
-    gen.update_dimensions(h_wndg_s= h_sw0 + h_sw0*iter_stator)
-    gen.maximize_k_fill_s()
+if 0:
+    # ============================== Initialize Iteration ==============================
+    p = 30
+    job_init_time = time.time()
     
-    # ============================== Rotor Winding Iteration ==============================
-    lst_rotor_iter = []
-    for idx_rotor, iter_rotor in enumerate(np.linspace(0, n_times_r, n_iters_r)):      
-        gen.update_dimensions(h_wndg_r = h_rw0 + h_rw0*iter_rotor)
-        gen.maximize_k_fill_r()
+    gen = n7_Model(p, l_e, r_so, gn=gn, fw=fw, sw=sw)
+    h_pf = gn.h_pole_frame
+    gen.init_dimensions(h_yoke_s=h_pf, h_yoke_r=h_pf, 
+                        h_wndg_s=h_pf * 2.1, h_wndg_r=h_pf * 2.1)
+    gen.ks_d= 0.866
+    
+    # ============================== Stator Winding Iteration ==============================    
+    lst_stator_iter = []
+    h_sw0 = h_pf*2.1 #0.3 
+    h_rw0 = h_pf*2.1 #0.3
+    
+    n_iters_s = 9
+    n_times_s= 3
+    n_iters_r = 12
+    n_times_r=3 
+    
+    detail = 0.0001
+    verbose = False
+    
+    for idx_stator, iter_stator in enumerate(np.linspace(0, n_times_s, n_iters_s)):
+        gen.update_dimensions(h_wndg_s= h_sw0 + h_sw0*iter_stator)
+        gen.maximize_k_fill_s()
         
-        gen.apply_coil_sizes_and_lift_factor()
-        gen.adapt_yokes()
-        lst_rotor_iter.append(copy.deepcopy(gen))
-    # ============================== End Rotor Winding Iteration ==============================
-    lst_stator_iter.append(lst_rotor_iter)
-# ============================== End Stator Winding Iteration ==============================
-
-job_finish_time = time.time()
-job_runtime = job_finish_time - job_init_time
-total_runs = gen.runs
-sys.stdout.write(f"\rJob finished with {total_runs} solved models in {np.round(job_runtime,3)} seconds." + " "*80)
-sys.stdout.flush()
-
-##%% Plot iteration of h_sw and h_rw with lift factor
-fig = plt.figure(dpi=300, figsize=(6,7))
-ax1 = plt.subplot()
-plt.grid()
-
-for lst_rotor_iter in lst_stator_iter:
-    ax1.scatter([g.h_wndg_r for g in lst_rotor_iter], 
-             [g.P for g in lst_rotor_iter], 
-             label=f"h_sw= {np.round(lst_rotor_iter[0].h_wndg_s, 3)}")
-    ax1.plot([g.h_wndg_r for g in lst_rotor_iter], 
-             [g.P for g in lst_rotor_iter])
-
-ax1.set_xlabel("h_rw")
-ax1.set_ylabel("P")
-ax1.legend(loc="upper left")
-plt.title(f"Pole Pair Count p = {gen.p}")
-plt.show()
-
+        # ============================== Rotor Winding Iteration ==============================
+        lst_rotor_iter = []
+        for idx_rotor, iter_rotor in enumerate(np.linspace(0, n_times_r, n_iters_r)):      
+            gen.update_dimensions(h_wndg_r = h_rw0 + h_rw0*iter_rotor)
+            gen.maximize_k_fill_r()
+            
+            gen.apply_coil_sizes_and_lift_factor()
+            gen.adapt_yokes()
+            lst_rotor_iter.append(copy.deepcopy(gen))
+        # ============================== End Rotor Winding Iteration ==============================
+        lst_stator_iter.append(lst_rotor_iter)
+    # ============================== End Stator Winding Iteration ==============================
+    
+    job_finish_time = time.time()
+    job_runtime = job_finish_time - job_init_time
+    total_runs = gen.runs
+    sys.stdout.write(f"\rJob finished with {total_runs} solved models in {np.round(job_runtime,3)} seconds." + " "*80)
+    sys.stdout.flush()
+    
+    ##%% Plot iteration of h_sw and h_rw with lift factor
+    fig = plt.figure(dpi=300, figsize=(6,7))
+    ax1 = plt.subplot()
+    plt.grid()
+    
+    for lst_rotor_iter in lst_stator_iter:
+        ax1.scatter([g.h_wndg_r for g in lst_rotor_iter], 
+                 [g.P for g in lst_rotor_iter], 
+                 label=f"h_sw= {np.round(lst_rotor_iter[0].h_wndg_s, 3)}")
+        ax1.plot([g.h_wndg_r for g in lst_rotor_iter], 
+                 [g.P for g in lst_rotor_iter])
+    
+    ax1.set_xlabel("h_rw")
+    ax1.set_ylabel("P")
+    ax1.legend(loc="upper left")
+    plt.title(f"Pole Pair Count p = {gen.p}")
+    plt.show()
 
 #%% Pole Pair Iteration
 # ============================== Initialize Iteration ==============================
@@ -98,8 +98,8 @@ plt.show()
 job_init_time = time.time()
 total_runs = 0
 
-p = 20
-P_target = gen.gn.Pel_out
+p = 6
+P_target = gn.Pel_out
 
 # ============================== Pole Pair Iteration ==============================
 # lst_best_generators = []
@@ -160,6 +160,9 @@ for idx_stator, iter_stator in enumerate(np.linspace(0, n_times_s, n_iters_s)):
             
             streak_pos = 1
             streak_neg = 1
+            n_Pel_out = 40
+            overshoot = False
+            old = copy.copy(gen.h_wndg_r)
             for idx_Pel_out in range(100):
                 if (gen.P >= (1-detail)*P_target and gen.P <= (1+detail)*P_target):
                     sys.stdout.write(f"\rComputing Pole Pair: {p} - Stator Iteration: {idx_stator+1} / {n_iters_s} - Rotor Iterations: {idx_rotor+1} / {n_iters_r} - Converged.         ")
@@ -173,10 +176,28 @@ for idx_stator, iter_stator in enumerate(np.linspace(0, n_times_s, n_iters_s)):
                 sys.stdout.write(f"\rComputing Pole Pair: {p} - Stator Iteration: {idx_stator+1} / {n_iters_s} - Rotor Iterations: {idx_rotor+1} / {n_iters_r} - Convergence Step {idx_Pel_out}         ")
                 sys.stdout.flush()
                 
+                n0 = (n_Pel_out - min(idx_Pel_out, n_Pel_out))/n_Pel_out
+                n1 = min(idx_Pel_out, n_Pel_out)/n_Pel_out
+                
+                if overshoot:
+                    if gen.P < P_target:
+                        h_rw = old - old * (n0*0.0002 + n1*0.00002)
+                    else:
+                        h_rw = old + old * (n0*0.00015 + n1*0.000015)
+                    gen.update_dimensions(h_wndg_r = h_rw)
+                    gen.maximize_k_fill_r()
+                    gen.apply_coil_sizes_and_lift_factor()
+                    gen.adapt_yokes()   
+                    overshoot = False
+                    if verbose:
+                        x.append(idx_Pel_out)
+                        y.append(gen.P)
+                    continue
+                
                 old = copy.copy(gen.h_wndg_r)
                 if gen.P > P_target:
                     streak_neg = 1                    
-                    h_rw = gen.h_wndg_r - gen.h_wndg_r * 0.0005 * streak_pos
+                    h_rw = gen.h_wndg_r - gen.h_wndg_r * ((n0*0.002 + n1*0.00002) * streak_pos)
                     streak_pos *= 2
                     
                     gen.update_dimensions(h_wndg_r = h_rw)
@@ -186,14 +207,11 @@ for idx_stator, iter_stator in enumerate(np.linspace(0, n_times_s, n_iters_s)):
                     
                     if gen.P < P_target:
                         streak_pos = 1
-                        gen.update_dimensions(h_wndg_r = old -old * 0.00005)
-                        gen.maximize_k_fill_r()
-                        gen.apply_coil_sizes_and_lift_factor()
-                        gen.adapt_yokes()
-
+                        overshoot = True  
+                        continue
                 else:
                     streak_pos = 1
-                    h_rw = gen.h_wndg_r + gen.h_wndg_r * 0.0007 * streak_neg
+                    h_rw = gen.h_wndg_r + gen.h_wndg_r * ((n0*0.0015 + n1*0.000015) * streak_neg)
                     streak_neg *=2
 
                     gen.update_dimensions(h_wndg_r = h_rw)
@@ -203,14 +221,8 @@ for idx_stator, iter_stator in enumerate(np.linspace(0, n_times_s, n_iters_s)):
                     
                     if gen.P > P_target:
                         streak_neg = 1 
-                        gen.update_dimensions(h_wndg_r = old - old * 0.00008)
-                        gen.maximize_k_fill_r()
-                        gen.apply_coil_sizes_and_lift_factor()
-                        gen.adapt_yokes()    
-
-
-                
-                            
+                        overshoot = True   
+                        continue
                 if verbose:
                     x.append(idx_Pel_out)
                     y.append(gen.P)
