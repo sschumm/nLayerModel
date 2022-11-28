c9f9b03bcacdb7f40dce51389906061f6a1cbe0b
sschumm <sven.schumm7@gmail.com>
2022-11-28 21:38:49 +0100

    Added data to thesis/visualize for 77K

--- thesis/09_reading_csv.py
+++ thesis/09_reading_csv.py
@@ -5,9 +5,13 @@ import pathlib as pl
 import tikzplotlib as tkz
 import matplotlib.pyplot as plt
 
-filename = "InducedVoltages_UVW"
-path = pl.Path(__file__).parent / "visualize" / (filename + ".csv")
+directory = "1128_2111_data_77K_p30_r2"
 
+
+#%% --- voltages ---
+
+filename = "HB_induced_voltages"
+path = pl.Path(__file__).parent / "visualize" / directory / (filename + ".csv")
 
 columns = ["Time", "U", "V", "W"]
 df = pd.read_csv(path, usecols=columns) # df = dataframe
@@ -23,8 +27,50 @@ ax1.set_ylabel("Voltage")
 ax1.plot(df.Time, df.U, color = "blue", label="U")
 ax1.plot(df.Time, df.V, color = "red", label="V")
 ax1.plot(df.Time, df.W, color = "green", label="W")
-ax1.legend()
+# ax1.legend()
 
 tkz.clean_figure()
 # tkz.save("aus_Ui_HB_77K_p30.tex")
 
+
+#%% --- current densities ---
+
+filename = "HB_current_densities"
+path = pl.Path(__file__).parent / "visualize" / directory / (filename + ".csv")
+
+columns = ["Time", "U", "V", "W"]
+df = pd.read_csv(path, usecols=columns) # df = dataframe
+
+fig = plt.figure(dpi=300, figsize=(10,7))
+ax1 = plt.subplot()
+plt.grid()
+
+ax1.set_xlabel("Time")
+ax1.set_ylabel("J")
+ax1.plot(df.Time, df.U, color = "blue", label="U")
+ax1.plot(df.Time, df.V, color = "red", label="V")
+ax1.plot(df.Time, df.W, color = "green", label="W")
+# ax1.legend()
+
+#%% --- torque ---
+
+filename = "HB_torque_force_calculation"
+path = pl.Path(__file__).parent / "visualize" / directory / (filename + ".csv")
+
+columns = ["Time", "M"]
+df = pd.read_csv(path, usecols=columns) # df = dataframe
+
+fig = plt.figure(dpi=300, figsize=(10,7))
+ax1 = plt.subplot()
+plt.grid()
+
+ax1.set_xlabel("Time")
+ax1.set_ylabel("M")
+ax1.plot(df.Time, df.M, color = "blue", label="M")
+
+filename = "HB_torque_via_maxwell"
+path = pl.Path(__file__).parent / "visualize" / directory / (filename + ".csv")
+
+columns = ["Time", "M"]
+df = pd.read_csv(path, usecols=columns) # df = dataframe
+ax1.plot(df.Time, df.M, color = "red", label="M")
