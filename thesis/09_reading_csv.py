286321c7a83c62b5aedc5863b373ceb242e4a5d4
sschumm <sven.schumm7@gmail.com>
2022-12-01 12:52:00 +0100

    Updated 09_reading_csv.py to plot in Meganewton.

--- thesis/09_reading_csv.py
+++ thesis/09_reading_csv.py
@@ -27,10 +27,10 @@ ax1.set_ylabel("Voltage")
 ax1.plot(df.Time, df.U, color = "blue", label="U")
 ax1.plot(df.Time, df.V, color = "red", label="V")
 ax1.plot(df.Time, df.W, color = "green", label="W")
-# ax1.legend()
+ax1.legend()
 
 tkz.clean_figure()
-# tkz.save("aus_Ui_HB_77K_p30.tex")
+tkz.save("aus_comsol_Ui_HB_77K_p30.tex")
 
 
 #%% --- current densities ---
@@ -50,7 +50,10 @@ ax1.set_ylabel("J")
 ax1.plot(df.Time, df.U, color = "blue", label="U")
 ax1.plot(df.Time, df.V, color = "red", label="V")
 ax1.plot(df.Time, df.W, color = "green", label="W")
-# ax1.legend()
+ax1.legend()
+
+tkz.clean_figure()
+tkz.save("aus_comsol_Je_HB_77K_p30.tex")
 
 #%% --- torque ---
 
@@ -66,12 +69,15 @@ plt.grid()
 
 ax1.set_xlabel("Time")
 ax1.set_ylabel("M")
-ax1.set_ylim(0, 9e6)
-ax1.plot(df.Time, df.M, color = "blue", label="M")
+ax1.set_ylim(7, 7.2)
+ax1.plot(df.Time, [M*1e-6 for M in df.M], color = "blue", label="M")
+
+tkz.clean_figure()
+tkz.save("aus_comsol_Me_HB_77K_p30.tex")
 
 filename = "HB_torque_via_maxwell"
 path = pl.Path(__file__).parent / "visualize" / directory / (filename + ".csv")
 
 columns = ["Time", "M"]
 df = pd.read_csv(path, usecols=columns) # df = dataframe
-ax1.plot(df.Time, df.M, color = "red", label="M")
+# ax1.plot(df.Time, df.M, color = "red", label="M")
